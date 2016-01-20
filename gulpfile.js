0a321683329057386ece229178e791cea05629da
Azat S <to@azat.io>
2016-01-20 15:44:33 +0300

    Add Sass and PostCSS Import

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var cssMqpacker = require('css-mqpacker');
 var cssNext = require('postcss-cssnext');
 var focus = require('postcss-focus');
 var gulp = require('gulp');
+var atImport = require('postcss-import')
 var nano = require('gulp-cssnano');
 var postcss = require('gulp-postcss');
 var precss = require('precss');
 var propertySorter = require('css-property-sorter');
 var px2Rem = require('postcss-pxtorem');
+var sass = require('gulp-sass');
 var size = require('postcss-size');
 var short = require ('postcss-short');
 var watch = require('gulp-watch');
@@ -19,10 +21,20 @@ gulp.task('default', ['server'], function() {
   gulp.watch('src/css/*.css', function(event) {
     gulp.run('css');
   });
+  gulp.watch('src/sass/*.scss', function(event) {
+    gulp.run('sass');
+  });
+});
+
+gulp.task('sass', function () {
+  gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css/assets/'));
 });
 
 gulp.task('css', function () {
   var processors = [
+    atImport,
     colorShort,
     focus,
     precss,
